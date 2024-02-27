81f725425d37384a3ed772be0973a95b55a4e8b0
charmxyz <firdaussaffann@gmail.com>
2024-02-27 19:36:37 +0000

    fixed error on js

--- script.js
+++ script.js
@@ -12,18 +12,24 @@ const button3 = document.querySelector("#button3");
 
 const locations = [
   {name:"town square",
-  "button text": ["Go to Store", "Go toCave", "Fight Dragon"],
+  "button text": ["Go to store", "Go to cave", "Fight dragon"],
   "button functions": [goStore, goCave, fightDragon],
   "text": "You are in the town square. There is a sign that says \'Store\'"},
   {name:"store",
-  "button text": ["Buy 10 Health (10 Gold)", "Buy Weapon (30 Gold)", "Go to Town Square"],
+  "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
   "button functions": [buyHealth, buyWeapon, goTown],
   "text": "You enter the store."},
   {name:"cave",
-  "button text": ["Fight slime", "Fight fanged beast", "Go to Town Square"],
+  "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
   "button functions": [fightSlime, fightFangedBeast, goTown],
   "text": "You enter the cave. You see some monsters."},
 ];
+
+// initialize button
+
+button1.onclick = goStore;
+button2.onclick = goCave;
+button3.onclick = goTown;
 
 function update(loc){
   button1.innerText = loc["button text"][0];
@@ -32,23 +38,37 @@ function update(loc){
   button1.onclick = loc["button functions"][0];
   button2.onclick = loc["button functions"][1];
   button3.onclick = loc["button functions"][2];
-  text.innerText = loc["text"];
+  text.innerText = loc.text;
 }
 
 function goStore(){
-  update(locations[0]);
-}
-
-function goCave(){
   update(locations[1]);
 }
 
-function goTown(){
+function goCave(){
   update(locations[2]);
 }
 
-// initialize button
+function goTown(){
+  update(locations[0]);
+}
 
-button1.onclick = goStore();
-button2.onclick = goCave();
-button3.onclick = goTown();
+function fightDragon(){
+  console.log("fighting dragon");
+}
+
+function buyHealth(){
+  console.log("buying health");
+}
+
+function buyWeapon(){
+  console.log("buying weapon");
+}
+
+function fightSlime(){
+  console.log("fighting slime");
+}
+
+function fightFangedBeast(){
+  console.log("fighting fanged beast");
+}

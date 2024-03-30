fc2ad56c5002652444c4194e989f828da0f51fa9
charmxyz <firdaussaffann@gmail.com>
2024-03-30 13:56:28 +0000

    update

--- script.js
+++ script.js
@@ -47,6 +47,12 @@ const locations = [
     "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
     "button functions": [fightSlime, fightBeast, goTown],
     text: "You enter the cave. You see some monsters."
+  },
+  {
+    name: "fight",
+    "button text": ["Attack", "Dodge", "Run"],
+    "button functions": [attack, dodge, goTown],
+    text: "You are fighting a monster."
   }
 ];
 
@@ -134,9 +140,19 @@ function fightSlime() {
 }
 
 function fightBeast() {
-
+  fighting = 1;
+  goFight()
 }
 
 function goFight() {
+  fighting = 2;
+  goFight()
+}
 
+function attack(){
+
+}
+
+function dodge(){
+  
 }
